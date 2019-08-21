90bd005a13780d08cac3144318a8e4ad594e581c
Milan Šuka <milan.suka@outlook.com>
2019-08-21 09:51:53 +0200

    fix

--- cypress/support/reporter.js
+++ cypress/support/reporter.js
@@ -2,22 +2,22 @@ const report = require('multiple-cucumber-html-reporter');
 
 report.generate({
     jsonDir: './cypress/test-results/cucumber-json/',
-   reportPath: './cypress/output/',
-   metadata:{
-       browser: {
-           name: 'chrome',
-           version: '67'
-       },
-       device: 'Local test machine',
-       platform: {
-           name: 'ubuntu',
-           version: '16.04'
-       }
-   },
-   customData: {
-       title: 'Run tests',
-       data: [
-           {label: 'Project', value: 'Trainsmart project'}
-       ]
-   }
+    reportPath: './cypress/output/',
+    metadata: {
+        browser: {
+            name: 'Chrome',
+            version: '69'
+        },
+        device: 'Local test machine',
+        platform: {
+            name: 'ubuntu',
+            version: '16.04'
+        }
+    },
+    customData: {
+        title: 'Run tests',
+        data: [
+            { label: 'Project', value: 'Trainsmart project' }
+        ]
+    }
 });
